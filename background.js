d5b8137c202c4a64a5a262d2983c0a43c09adde7
Ivan Baksheev <dczpix@gmail.com>
2019-08-29 01:04:31 +0700

    New version from new repo (copied here) v1.1

--- background.js
+++ background.js
@@ -19,31 +19,12 @@ function get(url, callback) {
     };
 }
 
-function linkify(inputText) {
-    var replacedText, replacePattern1, replacePattern2, replacePattern3;
-    const customPart = 'class="yt-simple-endpoint style-scope yt-formatted-string" spellcheck="false"  rel="nofollow"';
-
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a ' + customPart + ' href="$1" target="_blank">$1</a>');
-
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a ' + customPart + ' href="http://$2" target="_blank">$2</a>');
-
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a ' + customPart + ' href="mailto:$1">$1</a>');
-
-    return replacedText;
-}
-
 function untranslateCurrentVideo() {
     let realTitle = document.querySelector(".ytp-title-link").innerText;
     let translatedTitleElement = document.querySelector(".title").children[0];
 
     if (!realTitle || !translatedTitleElement) {
-        // Do noting if video is not loaded yet
+        // Do nothing if video is not loaded yet
         return;
     }
     if (realTitle === translatedTitleElement.innerText) {
@@ -51,7 +32,7 @@ function untranslateCurrentVideo() {
         return;
     }
     // let realDescription = window["ytInitialPlayerResponse"].videoDetails.shortDescription;
-    let translatedDescriptionElement = document.querySelector(".content.style-scope.ytd-video-secondary-info-renderer");
+    // let translatedDescriptionElement = document.querySelector(".content.style-scope.ytd-video-secondary-info-renderer");
 
     translatedTitleElement.innerText = realTitle;
     // translatedDescriptionElement.innerHTML = linkify(realDescription);
@@ -61,9 +42,11 @@ function untranslateOtherVideos() {
     function untranslateArray(otherVideos) {
         for (let i = 0; i < otherVideos.length; i++) {
             let video = otherVideos[i];
-            if (!video.untranslatedByExtension) { // do not request same video multiply times
+            let videoId = video.querySelector('#thumbnail').href;
+            if ((!video.untranslatedByExtension) || (video.untranslatedKey !== videoId)) { // do not request same video multiply times
                 let href = video.querySelector('a');
                 video.untranslatedByExtension = true;
+                video.untranslatedKey = videoId;
                 get('https://www.youtube.com/oembed?url=' + href.href, function (response) {
                     const title = JSON.parse(response.responseText).title;
                     video.querySelector('#video-title').innerText = title;
--- options.js
+++ options.js
@@ -8,9 +8,10 @@ function save_options() {
             disabled: disabled,
         }, function () {
             document.getElementById('disable-button').innerText = disabled ? 'Enable' : 'Disable';
+            document.getElementById('status').innerText = disabled ? 'Disabled' : 'Enabled';
+            document.getElementById('status').className = 'status ' + (disabled ? 'disabled' : 'enabled');
             document.getElementById('disable-button').className = disabled ? 'disabled' : 'enabled';
         });
-
     });
 }
 
@@ -19,7 +20,12 @@ function restore_options() {
         disabled: false
     }, function (items) {
         document.getElementById('disable-button').innerText = items.disabled ? 'Enable' : 'Disable';
-        document.getElementById('disable-button').className = disabled ? 'disabled' : 'enabled';
+        document.getElementById('status').innerText = items.disabled ? 'Disabled' : 'Enabled';
+        document.getElementById('status').className = 'status ' + (items.disabled ? 'disabled' : 'enabled');
+        document.getElementById('disable-button').className = items.disabled ? 'disabled' : 'enabled';
+        
+        document.getElementById('disable-button').innerText = items.disabled ? 'Enable' : 'Disable';
+        document.getElementById('disable-button').className = items.disabled ? 'disabled' : 'enabled';
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
